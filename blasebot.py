3ccac48b6f5555e6838f8a08164c793de71d1e6c
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2021-03-03 17:01:30 -0500

    Update blasebot.py
    This is a fully functioning Windows version for blasebot that will max bet on the higher win percentage team. This version is capable of being run 24hours however, as mentioned in the code comments; it is HIGHLY recommended that you manually zoom your automated web browser to 30% upon launching for the first time. 
    
    This code will function on most Linux distributions if you manually convert the email/password paths to Linux appropriate paths. 
    
    A Raspberry Pi ARMv7 version is also in the works but due to the stability of browsers on the Pi and lack of web driver compatibility, this has proven to be a challenging task.

--- blasebot.py
+++ blasebot.py
@@ -1,47 +1,143 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+
 #Essential for driver + global vars
 driver = webdriver.Firefox()
-driver.get('https://www.blaseball.com/login')
 betPage = 'https://www.blaseball.com/upcoming'
+shopPage = 'https://www.blaseball.com/shop'
+logOut = 'https://www.blaseball.com/auth/logout'
 
+#Provides Login Credentials for Blaseball.com (CANNOT USE GOOGLE/APPLE/FACEBOOK)
+#For best results it is HIGHLY recommended you manually zoom out your automated browser to 30%.
+#Currently working to hardcode this but zooming in/out is a known issue mentioned in the GeckoDriver docs.
 def Credentials():
-    #Credentials for Blaseball.com
-    loginEmail = open("/home/jeremyg/Scripts/Blasebot/BlasebotEmail.txt", "r")
-    loginPass = open("/home/jeremyg/Scripts/Blasebot/Blasebotpass.txt", "r")
-    #Email input
-    usrname = driver.find_element_by_name("username") # Finds the email box
+    #Credentials for Blaseball.com.
+    driver.get('https://www.blaseball.com/login')
+    driver.maximize_window()
+    time.sleep(2)
+    #Create two different textfiles with your blasebot credentials and provide their unique path.
+    loginEmail = open(r"C:\Path\To\Email\Text-File\BlasebotEmail.txt", mode='r')
+    loginPass = open(r"C:\Path\To\Password\Text-File\Blasebotpass.txt", mode='r')
+    #Email input:
+    time.sleep(10)
+    #Finds the email box.
+    usrname = driver.find_element_by_name("username")
+    time.sleep(3)
     usrname.send_keys(loginEmail.read())
-    #Password input
-    passwrd = driver.find_element_by_name("password") # Finds the password box
+    #Password input:
+    #Finds the password box.
+    passwrd = driver.find_element_by_name("password")
+    time.sleep(3)
     passwrd.send_keys(loginPass.read())
-def finishLogin():
-    entercreds = driver.find_element_by_class_name("Auth-Submit") # Finds the continue button
-    time.sleep(2) #Added delay to smooth login
+    time.sleep(3)
+    #Submits the login credentials.
+    entercreds = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div[2]/form/div[3]/input")
+    time.sleep(5)
     entercreds.click()
-def firstBet():
+
+#Decides which team has the better win-percentage and selects the better team.
+def logic():
+    time.sleep(3)
+    away = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[1]/div[2]/div[3]").text
+    home = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[2]/div[2]/div[3]").text
+    time.sleep(5)
+    convertAway = away.replace('%','')
+    convertHome = home.replace('%','')
+    awayPercentage = int(convertAway)
+    homePercentage = int(convertHome)
+    if awayPercentage > homePercentage:
+        awayTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[1]/div[2]/div[3]").click()
+    else:
+         homeTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[2]/div[2]/div[3]").click()
+
+#Places a max-bet on the favored team.
+def placeBet():
+    time.sleep(3)
+    driver.find_element_by_partial_link_text("Place a Bet").click()
+    time.sleep(1)
+    logic()
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[2]/div/a').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[4]/button').click()
+    time.sleep(5)
+    try:
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[4]/button')
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/button/svg/path').click()
+    except NoSuchElementException:
+        placeBet()
+
+#Currently not in use but can be added with main functions to beg when out of currency.
+def beg():
+    driver.get(shopPage)
+    time.sleep(3)
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div[9]/div[3]/div[2]/button').click() #BEG BUTTON
+    time.sleep(3)
+
+#Currently not in use but can be added with main functions to beg when out of currency.
+def checkCurrency():
+    currency = driver.find_element_by_xpath('/html/body/div[1]/div/nav/div[3]/div[1]/a').text
+    currentCurrency = int(currency)
+    if currentCurrency == 0:
+        beg()
+
+#Rotates every 120 seconds to a different site while waiting for bets.
+def waitingForBets():
+    driver.get('https://www.blaseball.com')
+    time.sleep(120)
+    driver.get('https://www.blaseball.com/standings')
+    time.sleep(120)
+    driver.get('https://www.blaseball.com/bulletin')
+    time.sleep(120)
+    driver.get(logOut)
+    time.sleep(10)
+    driver.get('https://twitter.com/blaseball')
+    time.sleep(120)
+    driver.get(logOut)
+    time.sleep(5)
+
+#Recalls the main functions when an exception occurs. 
+def recallStack():
+    Credentials()
     time.sleep(1)
     driver.get(betPage)
-    driver.find_element_by_link_text("Place a Bet").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("Bet-Form-Team-Name").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("Bet-Form-Inputs-Amount-MaxBet").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("Bet-Submit.btn.btn-success").click()
+    time.sleep(3)
+    try:
+        while True:
+            placeBet()
+    except NoSuchElementException:
+        waitingForBets()
+        time.sleep(120)
+        recallStack()
 
+print('''
+ ___    _                        ___           _
+(  _`\ (_ )                     (  _`\        ( )_
+| (_) ) | |    _ _   ___    __  | (_) )   _   | ,_)
+|  _ <' | |  /'_` )/',__) /'__`\|  _ <' /'_`\ | |
+| (_) ) | | ( (_| |\__, \(  ___/| (_) )( (_) )| |_
+(____/'(___)`\__,_)(____/`\____)(____/'`\___/'`\__)
+___________________________________________________
+               By:Shodo, HWS Devteam
+                  Windows Edition
+''')
 
-#GAME ONE HREF
-gameOne = "https://www.blaseball.com/bet/12d9ad4a-9873-496f-81e1-e54e476a4727"
-
-
-####PROGRAM-START####
-#####-BLASEBOT-######
-##WWW.BLASEBALL.COM##
-Credentials()
-finishLogin()
-games = 0
-while games < 10:
-    firstBet()
+try:
+    Credentials()
+    time.sleep(5)
+    driver.get(betPage)
+    time.sleep(5)
+    try:
+        while True:
+            placeBet()
+    except NoSuchElementException:
+        #checkCurrency()
+        waitingForBets()
+        time.sleep(120) #15 mins = 900 sec
+        recallStack()
+except NoSuchElementException:
+    driver.get(logOut)
+    time.sleep(5)
+    recallStack()
