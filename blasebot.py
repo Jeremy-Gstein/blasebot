b74f9c8299a997648ac066e56352547e0a79859e
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2021-06-14 22:14:53 -0400

    Update blasebot.py
    Windows Blasebot install:
    1. Install the latest version of Python from https://www.python.org/downloads/
    2. Open a command prompt (Wndows Key + R + enter cmd in the text box)
    3. Install the latest version of Firefox.
    3. Use pip to install the latest version of Selenium "pip install selenium"
    4. Use pip to install the latest version of Geckodriver "pip install geckodriver-autoinstaller"
    5. Download the latest version of Blasebot from the github repo (https://github.com/Jeremy-Gstein/blasebot)
    6. Change/create the loginEmail and LoginPass files and associate them with their correct paths.

--- blasebot.py
+++ blasebot.py
@@ -10,80 +10,81 @@ shopPage = 'https://www.blaseball.com/shop'
 logOut = 'https://www.blaseball.com/auth/logout'
 
 #Provides Login Credentials for Blaseball.com (CANNOT USE GOOGLE/APPLE/FACEBOOK)
-#For best results it is HIGHLY recommended you manually zoom out your automated browser to 30%.
-#Currently working to hardcode this but zooming in/out is a known issue mentioned in the GeckoDriver docs.
 def Credentials():
-    #Credentials for Blaseball.com.
+    #Credentials for Blaseball.com
     driver.get('https://www.blaseball.com/login')
     driver.maximize_window()
     time.sleep(2)
-    #Create two different textfiles with your blasebot credentials and provide their unique path.
-    loginEmail = open(r"C:\Path\To\Email\Text-File\BlasebotEmail.txt", mode='r')
-    loginPass = open(r"C:\Path\To\Password\Text-File\Blasebotpass.txt", mode='r')
-    #Email input:
-    time.sleep(10)
-    #Finds the email box.
-    usrname = driver.find_element_by_name("username")
+    #INSERT LOCAL PATH TO 2 SEPERATE TEXT-FILES WITH BLASEBALL CREDENTIALS 
+    #Example Email PATH 
+    #loginEmail = open(r"C:\Users\'User-Name'\Documents\Blasebot\BlasebotEmail.txt", mode='r')
+    #loginPass = open(r"C:\Users\'User-name'\Documents\Blasebot\Blasebotpass.txt", mode='r')
+    #Email input
+    time.sleep(3)
+    #Finds the email box
+    #usrname = driver.find_element_by_name("username")
+    usrname = driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div/div[2]/form/div[1]/input')
     time.sleep(3)
     usrname.send_keys(loginEmail.read())
-    #Password input:
-    #Finds the password box.
-    passwrd = driver.find_element_by_name("password")
+    #Password input
+    #Finds the password box
+    #passwrd = driver.find_element_by_name("password")
+    passwrd = driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div/div[2]/form/div[2]/input')
     time.sleep(3)
     passwrd.send_keys(loginPass.read())
     time.sleep(3)
-    #Submits the login credentials.
+    #Submits the login credentials
     entercreds = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div[2]/form/div[3]/input")
-    time.sleep(5)
+    time.sleep(3)
     entercreds.click()
 
-#Decides which team has the better win-percentage and selects the better team.
+#Decides which team has the better win-percentage and selects the better team
 def logic():
     time.sleep(3)
-    away = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[1]/div[2]/div[3]").text
-    home = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[2]/div[2]/div[3]").text
-    time.sleep(5)
+    away = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[5]/div/form/div[1]/div[1]/div[2]/div[3]").text
+                                        
+    home = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[5]/div/form/div[1]/div[2]/div[2]/div[3]").text
+                                        
+    time.sleep(3)
     convertAway = away.replace('%','')
     convertHome = home.replace('%','')
     awayPercentage = int(convertAway)
     homePercentage = int(convertHome)
     if awayPercentage > homePercentage:
-        awayTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[1]/div[2]/div[3]").click()
+        awayTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[5]/div/form/div[1]/div[1]/div[2]/div[3]").click()
     else:
-         homeTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/form/div[1]/div[2]/div[2]/div[3]").click()
+         homeTeam = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[5]/div/form/div[1]/div[2]/div[2]/div[3]").click()
 
-#Places a max-bet on the favored team.
+#Places a max-bet on the favored team
 def placeBet():
     time.sleep(3)
     driver.find_element_by_partial_link_text("Place a Bet").click()
     time.sleep(1)
     logic()
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[2]/div/a').click()
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[5]/div/form/div[2]/div/a').click()
+                                
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[4]/button').click()
-    time.sleep(5)
+    driver.find_element_by_xpath('/html/body/div[1]/div/div/div[5]/div/form/div[4]/button').click()
+    time.sleep(3)
     try:
-        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/form/div[4]/button')
-        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/button/svg/path').click()
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[5]/div/form/div[4]/button').click()
     except NoSuchElementException:
         placeBet()
 
-#Currently not in use but can be added with main functions to beg when out of currency.
+
 def beg():
     driver.get(shopPage)
     time.sleep(3)
     driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div[9]/div[3]/div[2]/button').click() #BEG BUTTON
     time.sleep(3)
 
-#Currently not in use but can be added with main functions to beg when out of currency.
 def checkCurrency():
     currency = driver.find_element_by_xpath('/html/body/div[1]/div/nav/div[3]/div[1]/a').text
     currentCurrency = int(currency)
     if currentCurrency == 0:
         beg()
 
-#Rotates every 120 seconds to a different site while waiting for bets.
 def waitingForBets():
     driver.get('https://www.blaseball.com')
     time.sleep(120)
@@ -98,7 +99,7 @@ def waitingForBets():
     driver.get(logOut)
     time.sleep(5)
 
-#Recalls the main functions when an exception occurs. 
+
 def recallStack():
     Credentials()
     time.sleep(1)
@@ -111,7 +112,6 @@ def recallStack():
         waitingForBets()
         time.sleep(120)
         recallStack()
-
 print('''
  ___    _                        ___           _
 (  _`\ (_ )                     (  _`\        ( )_
@@ -123,6 +123,8 @@ ___________________________________________________
                By:Shodo, HWS Devteam
                   Windows Edition
 ''')
+#The Selenium Docs specify that leaving/closing the python executable may still leave ports open.
+#It is HIGHLY recomended you close blasebot this way for security reasons.
 
 try:
     Credentials()
@@ -135,6 +137,7 @@ try:
     except NoSuchElementException:
         #checkCurrency()
         waitingForBets()
+        #driver.get(logOut)
         time.sleep(120) #15 mins = 900 sec
         recallStack()
 except NoSuchElementException:
